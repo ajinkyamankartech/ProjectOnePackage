bd0bec20747eddc26790282362495c64a7b33d18
Ajinkya Rasik Mankar <crazyboy3733@gmail.com>
2024-01-14 02:26:55 +0530

    removed input error

--- Runtime/Scripts/UI/MetaGameController.cs
+++ Runtime/Scripts/UI/MetaGameController.cs
@@ -61,13 +61,13 @@ namespace Platformer.UI
             this.showMainCanvas = show;
         }
 
-        void Update()
-        {
-            if (Input.GetButtonDown("Menu"))
-            {
-                ToggleMainMenu(show: !showMainCanvas);
-            }
-        }
+        // void Update()
+        // {
+        //     if (Input.GetButtonDown("Menu"))
+        //     {
+        //         ToggleMainMenu(show: !showMainCanvas);
+        //     }
+        // }
 
     }
 }
